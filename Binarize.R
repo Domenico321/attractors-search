7c328bcf0c13f2c0afcd74387ecc3c8d9eee1789
Domenico321 <81741322+Domenico321@users.noreply.github.com>
2021-04-17 17:27:13 +0200

    Update Binarize.R

--- Binarize.R
+++ Binarize.R
@@ -1,6 +1,6 @@
 install.packages("Binarize")
 library("Binarize")
-dati=read.table("revision.txt", header=T)
+dati=read.table("TRIB3.txt", header=T)
 str(dati)
 result=binarize.BASC(dati[,"TRIB3"], method="B", tau=0.1)
 print(result)
